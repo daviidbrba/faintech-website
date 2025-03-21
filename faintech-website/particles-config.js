f19d8bb5af15cee15dabce6b2b1b44cfdc8d2238
daviidbrba <pequepoin@gmail.com>
2025-03-21 18:05:51 +0100

    Add files via upload

---
+++ faintech-website/particles-config.js
@@ -0,0 +1,24 @@
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 60 },
+    "size": { "value": 3 },
+    "color": { "value": "#00d1ff" },
+    "line_linked": {
+      "enable": true,
+      "distance": 130,
+      "color": "#00d1ff",
+      "opacity": 0.3,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.5
+    }
+  },
+  "interactivity": {
+    "events": {
+      "onhover": { "enable": true, "mode": "repulse" }
+    }
+  }
+});
